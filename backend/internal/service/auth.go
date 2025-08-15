3604ce3dca260e2d8ed0e821af25511d6980a44a
Meysam Bavi <meybsky@gmail.com>
2025-08-15 04:06:32 +0330

    feat(backend): Add authentication

---
+++ backend/internal/service/auth.go
@@ -0,0 +1,79 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"github.com/Rastaiha/bermudia/internal/config"
+	"github.com/Rastaiha/bermudia/internal/domain"
+	"github.com/golang-jwt/jwt/v5"
+	"log/slog"
+	"time"
+)
+
+type Auth struct {
+	userStore domain.UserStore
+	cfg       config.Config
+}
+
+func NewAuth(cfg config.Config, userStore domain.UserStore) *Auth {
+	return &Auth{cfg: cfg, userStore: userStore}
+}
+
+func (a *Auth) Login(ctx context.Context, username, password string) (string, error) {
+	user, err := a.userStore.GetByUsername(ctx, username)
+	if err != nil {
+		return "", err
+	}
+	if !domain.ValidatePassword(password, user.HashedPassword) {
+		return "", domain.ErrUserNotFound
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"user_id": fmt.Sprint(user.ID),
+		"iat":     float64(time.Now().UTC().UnixNano()) / 1e9,
+	})
+	tokenString, err := token.SignedString(a.cfg.TokenSigningKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign token: %w", err)
+	}
+	return tokenString, nil
+}
+
+func (a *Auth) ValidateToken(ctx context.Context, tokenStr string) (*domain.User, bool) {
+	token, err := jwt.Parse(
+		tokenStr,
+		func(token *jwt.Token) (interface{}, error) {
+			return a.cfg.TokenSigningKey, nil
+		},
+		jwt.WithValidMethods([]string{jwt.SigningMethodHS512.Alg()}),
+		jwt.WithIssuedAt(),
+	)
+	if err != nil {
+		return nil, false
+	}
+	if !token.Valid {
+		return nil, false
+	}
+	if iat, err := token.Claims.GetIssuedAt(); err != nil || time.Since(iat.Time) > 16*time.Hour {
+		return nil, false
+	}
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return nil, false
+	}
+	v, ok := claims["user_id"]
+	if !ok {
+		return nil, false
+	}
+	userIdStr, _ := v.(string)
+	var userId int32
+	_, err = fmt.Sscanf(userIdStr, "%d", &userId)
+	if err != nil {
+		return nil, false
+	}
+	user, err := a.userStore.Get(ctx, userId)
+	if err != nil {
+		slog.Error("failed to find valid user by id", userId)
+		return nil, false
+	}
+	return user, true
+}
--- backend/main.go
+++ backend/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"github.com/Rastaiha/bermudia/api/handler"
+	"github.com/Rastaiha/bermudia/internal/config"
+	"github.com/Rastaiha/bermudia/internal/domain"
 	"github.com/Rastaiha/bermudia/internal/repository"
 	"github.com/Rastaiha/bermudia/internal/service"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"log/slog"
 	"os"
 	"os/signal"
@@ -11,13 +15,28 @@ import (
 )
 
 func main() {
+	cfg := config.Load()
+
+	db, err := repository.ConnectToSqlite()
+	if err != nil {
+		log.Fatal("failed to connect to sqlite", err)
+	}
 	territoryRepo := repository.NewJSONTerritoryRepository()
 	islandRepo := repository.NewJSONIslandRepository()
+	userRepo, err := repository.NewSqlUser(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = domain.CreateMockData(userRepo, cfg.MockUsersPassword)
+	if err != nil {
+		log.Fatal("failed to create mock data", err)
+	}
 
+	authService := service.NewAuth(cfg, userRepo)
 	territoryService := service.NewTerritory(territoryRepo)
 	islandService := service.NewIsland(islandRepo)
 
-	h := handler.New(territoryService, islandService)
+	h := handler.New(authService, territoryService, islandService)
 
 	h.Start()
 
