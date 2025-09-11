2718aa9b8019f6e4d1569c10f8de8283421be430
Fardad86 <fardadgoodboy@gmail.com>
2025-09-11 15:26:34 +0330

    feat(frontend):simplize back buttton

--- frontend/src/components/BackButton.vue
+++ frontend/src/components/BackButton.vue
@@ -5,7 +5,7 @@
         @mouseleave="$emit('hideTooltip')"
     >
         <div id="go-back">
-            <img src="/images/ships/spaceship.png" alt="Go to the territory" />
+            <img src="/images/icons/back.png" alt="Go to the territory" />
         </div>
     </router-link>
 </template>
@@ -27,38 +27,26 @@ defineEmits(['showTooltip', 'hideTooltip']);
     z-index: 20;
     width: 10vw;
     height: 10vw;
-    bottom: 1.5rem;
-    right: 1.5rem;
+    top: 1.5rem;
+    left: 1.5rem;
     filter: drop-shadow(0 5px 20px rgba(0, 0, 0, 0.5));
-    animation: boat-animation 10s linear infinite;
 }
 
 #go-back img {
     object-fit: contain;
     width: 100%;
     height: 100%;
-    transition: transform 2s cubic-bezier(0.25, 1, 0.5, 1);
+    transition: transform 0.3s ease-in-out;
 }
 
 #go-back:hover img {
-    transform: scale(1.15) rotate(-5deg);
+    transform: scale(1.15);
 }
 
-@keyframes boat-animation {
-    0% {
-        transform: translate(0, 0) rotate(10deg);
-    }
-
-    35% {
-        transform: translate(10px, 5px) rotate(-10deg);
-    }
-
-    70% {
-        transform: translate(-10px, 5px) rotate(3deg);
-    }
-
-    100% {
-        transform: translate(0, 0) rotate(10deg);
+@media (min-width: 1024px) {
+    #go-back {
+        width: 5vw;
+        height: 5vw;
     }
 }
 </style>
