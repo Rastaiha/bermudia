8d08899d69d71cbbb76bb6cd1f1a216e7d728941
Roham Ghasemi <Roham8406@gmail.com>
2025-09-12 23:37:32 +0330

    noisified the stars

--- frontend/src/components/common/StarryNight.vue
+++ frontend/src/components/common/StarryNight.vue
@@ -34,30 +34,25 @@ const createStar = () => {
     star.className = 'star';
     star.style.cssText = `
         position: absolute;
-        background-color: #fff;
-        width: 1px;
-        height: 1px;
-        border-radius: 50%;
         top: ${Math.random() * 100}%;
         left: ${Math.random() * 100}%;
+        blur(1px) drop-shadow(0px 0px 10px yellow);
+    `;
+    const size = 6 + Math.random() * 8; // between 6px and 14px
+    star.style.width = `${size}px`;
+    star.style.height = `${size}px`;
+
+    star.innerHTML = `
+        <svg viewBox="0 0 122.88 122.88" xmlns="http://www.w3.org/2000/svg">
+            <path fill="white" d="M62.43,122.88h-1.98c0-16.15-6.04-30.27-18.11-42.34C30.27,68.47,16.16,62.43,0,62.43v-1.98 
+            c16.16,0,30.27-6.04,42.34-18.14C54.41,30.21,60.45,16.1,60.45,0h1.98c0,16.15,6.04,30.27,18.11,42.34 
+            c12.07,12.07,26.18,18.11,42.34,18.11v1.98c-16.15,0-30.27,6.04-42.34,18.11C68.47,92.61,62.43,106.72,62.43,122.88z"/>
+        </svg>
     `;
 
-    // Add the ::before pseudo-element effect using a real element
-    const twinkle = document.createElement('div');
-    twinkle.style.cssText = `
-        position: absolute;
-        top: -5px;
-        left: -5px;
-        width: 10px;
-        height: 10px;
-        border-radius: 50%;
-        background-color: rgba(255, 255, 255, 0.8);
-        opacity: 0;
-        animation: starTwinkle 2s infinite;
-        animation-delay: ${Math.random() * 2}s;
-    `;
+    star.style.animation = `starTwinkle 3s infinite`;
+    star.style.animationDelay = `${Math.random() * 2}s`;
 
-    star.appendChild(twinkle);
     return star;
 };
 
