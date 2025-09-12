c02e8b29523e1a08a6261d98954d4a68495b8605
AlieNiT <ali.blade.1380@gmail.com>
2025-09-12 16:17:59 +0330

    feat(frontend): Add starry night

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,7 +1,11 @@
 <script setup>
 import Login from '@/components/layout/LoginTemplate.vue';
+import StarryNight from '@/components/common/StarryNight.vue';
 </script>
 
 <template>
-    <Login />
+    <div class="relative min-h-screen overflow-hidden">
+        <StarryNight :star-count="200" :shooting-star-interval="6000" />
+        <Login />
+    </div>
 </template>
