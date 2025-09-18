93e06c9917d8d5d91877bbb2b592365cf62ee56b
ImFardad <fardadgoodboy@gmail.com>
2025-09-18 17:38:20 +0330

    feat(frontend): revert musics (#297)

--- frontend/src/services/audio/playlist.js
+++ frontend/src/services/audio/playlist.js
@@ -1,125 +1,124 @@
 export const playlist = [
-    // {
-    //     title: '1',
-    //     url: '/audio/1.mp3',
-    //     duration: 141,
-    // },
-    // {
-    //     title: '2',
-    //     url: '/audio/2.mp3',
-    //     duration: 151,
-    // },
-    // {
-    //     title: '3',
-    //     url: '/audio/3.mp3',
-    //     duration: 144,
-    // },
-    // {
-    //     title: '4',
-    //     url: '/audio/4.mp3',
-    //     duration: 154,
-    // },
-    // {
-    //     title: '5',
-    //     url: '/audio/5.mp3',
-    //     duration: 155,
-    // },
-    // {
-    //     title: '6',
-    //     url: '/audio/6.mp3',
-    //     duration: 144,
-    // },
-    // {
-    //     title: '7',
-    //     url: '/audio/7.mp3',
-    //     duration: 155,
-    // },
-    // {
-    //     title: '8',
-    //     url: '/audio/8.mp3',
-    //     duration: 157,
-    // },
-    // {
-    //     title: '9',
-    //     url: '/audio/9.mp3',
-    //     duration: 137,
-    // },
-    // {
-    //     title: '10',
-    //     url: '/audio/10.mp3',
-    //     duration: 145,
-    // },
-    // {
-    //     title: '11',
-    //     url: '/audio/11.mp3',
-    //     duration: 152,
-    // },
-    // {
-    //     title: '12',
-    //     url: '/audio/12.mp3',
-    //     duration: 129,
-    // },
-    // {
-    //     title: '13',
-    //     url: '/audio/13.mp3',
-    //     duration: 140,
-    // },
-    // {
-    //     title: '14',
-    //     url: '/audio/14.mp3',
-    //     duration: 33,
-    // },
-    // {
-    //     title: '15',
-    //     url: '/audio/15.mp3',
-    //     duration: 170,
-    // },
-    // {
-    //     title: '16',
-    //     url: '/audio/16.mp3',
-    //     duration: 157,
-    // },
-    // {
-    //     title: '17',
-    //     url: '/audio/17.mp3',
-    //     duration: 174,
-    // },
-    // {
-    //     title: '18',
-    //     url: '/audio/18.mp3',
-    //     duration: 120,
-    // },
-    // {
-    //     title: '19',
-    //     url: '/audio/19.mp3',
-    //     duration: 148,
-    // },
-    // {
-    //     title: '20',
-    //     url: '/audio/20.mp3',
-    //     duration: 144,
-    // },
-    // {
-    //     title: '21',
-    //     url: '/audio/21.mp3',
-    //     duration: 144,
-    // },
-    // {
-    //     title: '22',
-    //     url: '/audio/22.mp3',
-    //     duration: 154,
-    // },
-    // {
-    //     title: '23',
-    //     url: '/audio/23.mp3',
-    //     duration: 145,
-    // },
-    // {
-    //     title: '24',
-    //     url: '/audio/24.mp3',
-    //     duration: 172,
-    // },
-
+    {
+        title: '1',
+        url: '/audio/1.mp3',
+        duration: 141,
+    },
+    {
+        title: '2',
+        url: '/audio/2.mp3',
+        duration: 151,
+    },
+    {
+        title: '3',
+        url: '/audio/3.mp3',
+        duration: 144,
+    },
+    {
+        title: '4',
+        url: '/audio/4.mp3',
+        duration: 154,
+    },
+    {
+        title: '5',
+        url: '/audio/5.mp3',
+        duration: 155,
+    },
+    {
+        title: '6',
+        url: '/audio/6.mp3',
+        duration: 144,
+    },
+    {
+        title: '7',
+        url: '/audio/7.mp3',
+        duration: 155,
+    },
+    {
+        title: '8',
+        url: '/audio/8.mp3',
+        duration: 157,
+    },
+    {
+        title: '9',
+        url: '/audio/9.mp3',
+        duration: 137,
+    },
+    {
+        title: '10',
+        url: '/audio/10.mp3',
+        duration: 145,
+    },
+    {
+        title: '11',
+        url: '/audio/11.mp3',
+        duration: 152,
+    },
+    {
+        title: '12',
+        url: '/audio/12.mp3',
+        duration: 129,
+    },
+    {
+        title: '13',
+        url: '/audio/13.mp3',
+        duration: 140,
+    },
+    {
+        title: '14',
+        url: '/audio/14.mp3',
+        duration: 33,
+    },
+    {
+        title: '15',
+        url: '/audio/15.mp3',
+        duration: 170,
+    },
+    {
+        title: '16',
+        url: '/audio/16.mp3',
+        duration: 157,
+    },
+    {
+        title: '17',
+        url: '/audio/17.mp3',
+        duration: 174,
+    },
+    {
+        title: '18',
+        url: '/audio/18.mp3',
+        duration: 120,
+    },
+    {
+        title: '19',
+        url: '/audio/19.mp3',
+        duration: 148,
+    },
+    {
+        title: '20',
+        url: '/audio/20.mp3',
+        duration: 144,
+    },
+    {
+        title: '21',
+        url: '/audio/21.mp3',
+        duration: 144,
+    },
+    {
+        title: '22',
+        url: '/audio/22.mp3',
+        duration: 154,
+    },
+    {
+        title: '23',
+        url: '/audio/23.mp3',
+        duration: 145,
+    },
+    {
+        title: '24',
+        url: '/audio/24.mp3',
+        duration: 172,
+    },
     ///////////////////////////////////
     // {
     //     title: '25',
@@ -160,9 +159,9 @@ export const playlist = [
     //     url: '/audio/32.mp3',
     //     duration: 245,
     // },
-    {
-        title: '33',
-        url: '/audio/33.mp3',
-        duration: 230,
-    },
+    // {
+    //     title: '33',
+    //     url: '/audio/33.mp3',
+    //     duration: 230,
+    // },
 ];
